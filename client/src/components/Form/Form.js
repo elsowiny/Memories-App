18839d566d3184bd384fcf98a1c79f149663c2e9
Sherief Elsowiny <elsowiny@yahoo.com>
2021-05-17 22:07:09 -0400

    test and server and client are connected with mongodb

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,22 +2,27 @@ import React, { useState } from 'react';
 
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+import { createPost } from '../../actions/posts';
 import useStyles from './styles';
 const Form = () => {
     const [postData, setPostData] = useState({
         creator: '', title: '', message: '', tags: '', style: '', selectedFile: ''
     });
     const classes = useStyles();
-    const handleSubmit = () => {
+    const dispatch = useDispatch();
+    const handleSubmit = (e) => {
+            e.preventDefault();
 
+            dispatch(createPost(postData));
     }
     const clear = () =>{
 
     }
-    
+
     return (
         <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
             <Typography variant="h6">Create a Memory</Typography>
             <TextField  name="creator"  variant="outlined"  label="Creator"  fullWidth value={postData.creator} onChange={(e) => setPostData({...postData, creator: e.target.value})}/>
             <TextField  name="title"  variant="outlined"  label="Title"  fullWidth value={postData.title} onChange={(e) => setPostData({...postData, title: e.target.value})}/>
